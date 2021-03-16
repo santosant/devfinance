2118052caf330ef6346feb54949eaf3fbde46895
santosant <hiroto.anderson@gmail.com>
2021-03-16 19:30:03 -0300

    front end finalizado

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
-import { Container } from './styles'
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -7,14 +11,20 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit')
 
   return (
     <Modal 
     isOpen={isOpen}
     onRequestClose={onRequestClose}
-    overlayClassName="react-modal-content"
-    className="react-modal-overlay"
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
   > 
+
+    <button type="button" onClick={onRequestClose} className="react-modal-close">
+      <img src={closeImg} alt="Fechar modal"/>
+    </button>
+
     <Container>
     <h2>Cadastrar transação</h2>
 
@@ -26,6 +36,29 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       type="number"
       placeholder="Valor"
     />
+
+    <TransactionTypeContainer>
+      <RadioBox 
+          type="button"
+          onClick={() => {setType('deposit');}}
+          isActive={type === 'deposit'}
+          activeColor='green'
+      >
+        <img src={incomeImg} alt="Entrada" />
+        <span>Entrada</span>
+      </RadioBox>
+
+      <RadioBox 
+          type="button"
+          isActive={type === 'withdraw'}
+          onClick={() => {setType('withdraw');}}
+          activeColor='red'
+      >
+        <img src={outcomeImg} alt="Saída" />
+        <span>Saída</span>
+      </RadioBox>
+       
+    </TransactionTypeContainer>
 
     <input 
       placeholder="Categoria"
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -68,10 +68,24 @@ export const GlobalStyle = createGlobalStyle `
 
   .react-modal-content {
     width: 100%;
-    max-width: 576px;
+    max-width: 578px;
     background: var(--background);
     padding: 3rem;
     position: relative;
-    border-radius: 24rem;
+    border-radius: 0.24rem;
   }
-`
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    } 
+  }
+`;
